f9ea62931994c00b3c453e5d2f9ae1a52f1d2873
Yicheng Wei <77185679+davidYichengWei@users.noreply.github.com>
2022-12-03 22:27:00 -0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,33 +1,27 @@
 def testApp() {
-    echo "Executing pipeline for branch ${BRANCH_NAME}"
+    echo "Executing pipeline for branch master"
     echo "Testing the application..."
 }
 
 def buildJar() {
-    if (BRANCH_NAME == "master") {
-        echo "Building the application..."
-        sh "mvn package"
-    }
+    echo "Building the application..."
+    sh "mvn package"
 }
 
 def buildImage() {
-    if (BRANCH_NAME == "master") {
-        echo "Building the docker image..."
+    echo "Building the docker image..."
 
-        withCredentials([usernamePassword(credentialsId: 'DockerHub-credential', 
-            usernameVariable: 'USER', passwordVariable: 'PWD')]) 
-        {
-            sh "docker build -t yichengwei/demo-jenkins:jma-2.0 ."
-            sh "echo $PWD | docker login -u ${USER} --password-stdin"
-            sh "docker push yichengwei/demo-jenkins:jma-2.0"
-        }
+    withCredentials([usernamePassword(credentialsId: 'DockerHub-credential', 
+        usernameVariable: 'USER', passwordVariable: 'PWD')]) 
+    {
+        sh "docker build -t yichengwei/demo-jenkins:jma-3.1 ."
+        sh "echo $PWD | docker login -u ${USER} --password-stdin"
+        sh "docker push yichengwei/demo-jenkins:jma-3.1"
     }
 }
 
 def deployApp() {
-    if (BRANCH_NAME == "master") {
-        echo "Deploying the application..."
-    }
+    echo "Deploying the application..."
 }
 
 return this
